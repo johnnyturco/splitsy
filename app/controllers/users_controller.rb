07e65b584b1b9c8412bec141df91d4e51523bf94
Johnny <johnnyturco@me.com>
2023-01-22 12:51:45 -0700

    finished CRUD functionality for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
-# testing purposes to see users
+    # testing purposes to see users
     def index
         users = User.all
         render json: users, status: :ok
     end
 
-# "/login" route
+    # "/signup" route
     def create
         user = User.create!(user_params)
         session[:user_id] = user.id
@@ -18,9 +18,26 @@ class UsersController < ApplicationController
         render json: current_user
     end
 
+    def update
+        user = logged_in_user.find(params[:id])
+        user.update!(user_params)
+        render json: user, status: :accepted
+    end
+
+    def destroy
+        user = logged_in_user.find(params[:id])
+        user.destroy
+        head :no_content
+    end
+
     private
 
     def user_params
         params.permit(:first_name, :last_name, :username, :password, :password_confirmation, :venmo_username)
     end
+
+    def logged_in_user
+        User.where(:id => current_user.id)
+    end
+
 end
