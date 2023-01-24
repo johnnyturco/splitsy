22ee83a9e7505aee110ac4b588c39b4164e8d4ea
Johnny <johnnyturco@me.com>
2023-01-24 13:49:11 -0700

    fixed BillsProvider to re-render on new user login

--- client/src/context/BillsProvider.js
+++ client/src/context/BillsProvider.js
@@ -1,16 +1,20 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { useContext, createContext, useState, useEffect } from 'react';
+import { UserContext } from './UserProvider';
 
 const BillsContext = createContext();
 
 function BillsProvider({ children }) {
+  const { user } = useContext(UserContext);
 
   const [bills, setBills] = useState([]);
 
   useEffect(() => {
-    fetch(`/bills`)
-      .then((r) => r.json())
-      .then((bills) => setBills(bills));
-  }, [])
+    if (user){
+      fetch(`/bills`)
+        .then((r) => r.json())
+        .then((bills) => setBills(bills))
+    }
+  }, [user])
 
   return (
     <BillsContext.Provider value={{bills, setBills}}>
